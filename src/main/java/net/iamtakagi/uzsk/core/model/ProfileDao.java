d80b7471766e724423e04924669771abb276e256
iamtakagi <github@iamtakagi.net>
2024-09-01 07:12:21 +0900

    feat: upsert

--- src/main/java/net/iamtakagi/uzsk/core/model/ProfileDao.java
+++ src/main/java/net/iamtakagi/uzsk/core/model/ProfileDao.java
@@ -8,6 +8,7 @@ import net.iamtakagi.uzsk.core.model.entity.Profile;
 public interface ProfileDao {
     void insert(Profile profile);
 	void update(Profile profile);
+	void upsert(Profile profile);
 	void deleteById(Integer id);
 	Profile findById(Integer id);
 	Profile findByUUID(UUID uuid);
