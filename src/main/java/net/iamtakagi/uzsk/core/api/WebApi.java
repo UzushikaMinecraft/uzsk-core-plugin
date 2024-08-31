92cc91371ab130859430e0e0b1cf724fee9b19a2
iamtakagi <github@iamtakagi.net>
2024-09-01 03:46:03 +0900

    fix: getServerStatus by serverName

--- src/main/java/net/iamtakagi/uzsk/core/api/WebApi.java
+++ src/main/java/net/iamtakagi/uzsk/core/api/WebApi.java
@@ -59,9 +59,9 @@ class ServerStatus {
 
 public class WebApi {
 
-    public static ServerStatus getServerStatus() throws IOException {
+    public static ServerStatus getServerStatus(String serverName) throws IOException {
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(Core.getInstance().getCoreConfig().getWebApiSettings().getEndpointUrl()).build();
+        Request request = new Request.Builder().url(Core.getInstance().getCoreConfig().getWebApiSettings().getEndpointUrl() + "/servers/" + serverName).build();
         Response response = client.newCall(request).execute();
         String raw = response.body().string();
         Gson gson = new Gson();
